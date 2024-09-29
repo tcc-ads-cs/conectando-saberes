5735f585286a3ee4904a8d719d3e293c31008357
Ariel Martins <102676099+arielmartinsux@users.noreply.github.com>
2024-09-29 12:59:56 +0000

    1.3 - Adequação do CSS dos forms (cadastro/login)

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-//TODO: Inserir o preload das fontes
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
