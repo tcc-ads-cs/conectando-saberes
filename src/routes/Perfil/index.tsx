704fe65a5747812aef62e4a6158c7a8808386a6b
Ariel Martins <arielmartinsas@outlook.com>
2024-10-07 10:13:12 -0300

    8.1 - Adequação dos componentes da página de Perfil

--- src/routes/Perfil/index.tsx
+++ src/routes/Perfil/index.tsx
@@ -5,28 +5,32 @@ import Navbar from "../../components/Navbar";
 // Ideia: esse ser o componente que recebe um ID e renderiza o perfil em questão
 
 const Perfil: React.FC = () => {
+    var desc = 100;
+    let email = "ronald@ifsp.com";
+    
     return <>
         <Navbar />
         <div className="grid-alt">
             <main className="grid-a" style={{borderStyle:"solid"}}>
-                <Typography fontFamily={'poppins'} variant="h2">Nome.</Typography>
-                
-                <Typography fontFamily={'poppins'}>Descrição</Typography>
-                <Typography fontFamily={'poppins'}>Grau de escolaridade</Typography>
-                <Typography fontFamily={'poppins'}>Data de nascimento</Typography>
-                <Typography fontFamily={'poppins'}>Cidade + Estado</Typography>
-                <Typography fontFamily={'poppins'}>Seguidores</Typography>
-                <Typography fontFamily={'poppins'}>Seguindo</Typography>
+                <Typography fontFamily={'poppins'} variant="h2">{desc + " Nome"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Descrição"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Grau de escolaridade"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Data de nascimento"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Cidade + Estado"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Seguidores"}</Typography>
+                <Typography fontFamily={'poppins'}>{desc + " Seguindo"}</Typography>
 
                 {/* Broche 1
                 Broche 2 */}
                 
-                <img src="public/vite.svg" alt="" />
-                <button type="reset">Contato</button>
+                <img src="/vite.svg" alt="" />
+                
+                {/* Copiar para área de transferência */}
+                <button type="reset" onClick={() => {navigator.clipboard.writeText(email)}}>Contato</button>
             </main>
             <aside>
                 <div className="grid-e">
-                    testing
+                    {/* categorias */}
                 </div>
                 <div className="grid-b">
                     <MenuPrincipal />
