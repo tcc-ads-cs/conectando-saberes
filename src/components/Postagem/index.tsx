170d49334ffd000c591de08655eb0a87c7b108a8
Gabriel Martins <gabrielmartins_as@outlook.com>
2024-10-13 14:09:23 -0300

    8.3 - Adequação dos componentes da página de Perfil

--- src/components/Postagem/index.tsx
+++ src/components/Postagem/index.tsx
@@ -1,10 +1,3 @@
-import BtnInteracao from "../BtnInteracao";
-import Broche from "../Broche";
-
-import { Typography } from "@mui/material";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import FileOpenIcon from '@mui/icons-material/FileOpen';
-
 interface PostagemProps {
     post: string | any;
 }
@@ -13,8 +6,9 @@ const Postagem: React.FC<PostagemProps> = ({post}) => {
     switch (post.type) {
         case 0:
             //TODO: Ver com o Ronald sobre quais informações eu preciso pro posts!
-            return post.guid;
-        // Case 0 será um artigo com download, case 1 será artigo sem download e case 2 será tópico!
+            return post.textPost + " - (" + post.type + ")";
+        default:
+            return post.textPost + " - (" + post.type + ")";
     }
 }
 
