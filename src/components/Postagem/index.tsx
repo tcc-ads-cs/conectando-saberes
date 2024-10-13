021646616349e883ad0825b55ce26651253b8cd7
Ariel Martins <arielmartinsas@outlook.com>
2024-10-12 23:28:32 -0300

    1.2 - Adição do Axios

--- src/components/Postagem/index.tsx
+++ src/components/Postagem/index.tsx
@@ -14,7 +14,7 @@ const Postagem: React.FC<PostagemProps> = ({post}) => {
         case 0:
             //TODO: Ver com o Ronald sobre quais informações eu preciso pro posts!
             return post.guid;
-        // Case 0 será um artigo, case 1 será um tópico!
+        // Case 0 será um artigo com download, case 1 será artigo sem download e case 2 será tópico!
     }
 }
 
