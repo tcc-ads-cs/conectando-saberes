3790de6ad28de4142dd03770c4de8f633d4916b0
Gabriel Martins <gabrielmartins_as@outlook.com>
2024-10-14 09:35:47 -0300

    8.4 - Adequação dos componentes da página de Perfil

--- src/components/Postagem/index.tsx
+++ src/components/Postagem/index.tsx
@@ -5,7 +5,6 @@ interface PostagemProps {
 const Postagem: React.FC<PostagemProps> = ({post}) => {
     switch (post.type) {
         case 0:
-            //TODO: Ver com o Ronald sobre quais informações eu preciso pro posts!
             return post.textPost + " - (" + post.type + ")";
         default:
             return post.textPost + " - (" + post.type + ")";
